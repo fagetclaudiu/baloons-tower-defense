1f92b962e21106106d81994adf20c40f96ea36f2
Claudiu <fagetclaudiu@gmail.com>
2020-09-15 14:53:26 +0300

    Bubble Tower v0.1

---
+++ Bubble Tower Defense/Assets/Scripts/BreakOnTap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakOnTap : MonoBehaviour
+{
+
+
+    void distrugere(){
+         Destroy(this.gameObject);
+    }
+    public int health = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    if (Input.GetMouseButtonDown(0)){
+    health = 0;
+    }
+
+    if (health == 0){
+    distrugere();
+    }
+    }
+}
---
+++ Bubble Tower Defense/Assets/Scripts/InstantiateBubble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBubble : MonoBehaviour
+{
+
+    public GameObject bubblePrefab;
+
+
+    void BubbleSpawn(){
+        
+        float xRandom = Random.Range(-Screen.width, Screen.width);
+
+        GameObject newbubble = Instantiate(bubblePrefab, new Vector3(xRandom/2.5f, 0, 0), Quaternion.identity) as GameObject;
+        newbubble.transform.SetParent (GameObject.FindGameObjectWithTag("Canvas").transform, false);
+
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating ("BubbleSpawn", 0.0f, 0.5f);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
